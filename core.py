08de2fde41c0adec87126c40c02e8e277fb91f9d
afonso coelho <afonsogcoelho@gmail.com>
2024-07-19 15:12:18 -0400

    adicioando getters and setters

--- core.py
+++ core.py
@@ -1,15 +1,54 @@
 class Movie:
     def __init__(self, name, year, duration):
-        self.name = name.title()
+        self.__name = name.title()
         self.year = year
         self.duration = duration
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def add_likes(self):
+        self.__likes += 1
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, new_name):
+        self.nome = new_name.title()
+
+   
 
 
 class Series:
-     def __init__(self, name, year, seasons):
-        self.name = name.title()
+    def __init__(self, name, year, seasons):
+        self.__name = name.title()
         self.year = year
         self.seasons = seasons
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def add_likes(self):
+        self.__likes += 1
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, new_name):
+        self.nome = new_name.title()
+
+
+    
+
+
 
 
 
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ avengers = Movie('avengers - endgame', 2011, 100)
 tua = Series('The umbrella academy', 2016, 3)
 
 
-print(f'The movie is caled {avengers.name}, was realese in {avengers.year}, and is {avengers.duration} min long')
+print(f'The movie is caled {avengers.name}, was realese in {avengers.year}, and is {avengers.duration} min long\nLikes - {avengers.likes}')
 
-print(f'The Series is caled {tua.name}, was realese in {tua.year}, and have {tua.seasons} seasons')
+print('#############################################################################################')
+
+print(f'The Series is caled {tua.name}, was realese in {tua.year}, and have {tua.seasons} seasons.\nLikes - {tua.likes}')
