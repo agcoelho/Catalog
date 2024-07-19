630575d658a04a364a6cb9039f7839dddf0f4cdf
afonso coelho <afonsogcoelho@gmail.com>
2024-07-19 17:12:55 -0400

    organizando as classes

--- core.py
+++ core.py
@@ -18,6 +18,12 @@ class Program:
     @name.setter
     def name(self, new_name):
         self.nome = new_name.title()
+
+    def show(self):
+        print(f'Title -> {self._name}\nYear -> {self.year}\nLikes -> {self._likes}')
+
+    def line_break():
+        print('\n#############################################################################################\n')
        
 
 
@@ -26,6 +32,9 @@ class Movie(Program):
     def __init__(self, name, year, duration):
         super().__init__(name, year)
         self.duration = duration
+
+    def show(self):
+        print(f'Title -> {self._name}\nYear -> {self.year}\nDuration -> {self.duration} Min\nLikes -> {self._likes}')
         
 
    
@@ -35,7 +44,14 @@ class Series(Program):
     def __init__(self, name, year, seasons):
         super().__init__(name, year)
         self.seasons = seasons
+
+
+    def show(self):
+        print(f'Title -> {self._name}\nYear -> {self.year}\nSeasons -> {self.seasons}\nLikes -> {self._likes}')
         
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from core import Movie, Series
+from core import Movie, Series, Program
 
 avengers = Movie('avengers - endgame', 2011, 100)
 tua = Series('The umbrella academy', 2016, 3)
@@ -9,16 +9,8 @@ avengers.add_likes()
 avengers.add_likes()
 tua.add_likes()
 
-
-
-print(f'The movie is caled {avengers.name}, was realese in {avengers.year}, and is {avengers.duration} min long\nLikes - {avengers.likes}')
-
-print('#############################################################################################')
-
-print(f'The Series is caled {tua.name}, was realese in {tua.year}, and have {tua.seasons} seasons.\nLikes - {tua.likes}')
-
-
 movies_series = [avengers, tua]
 
 for program in movies_series:
-    print(f'Title -> {program.name}\nYear -> {program.year}\nLikes -> {program.likes}\n') 
+    program.show()
+    Program.line_break()
